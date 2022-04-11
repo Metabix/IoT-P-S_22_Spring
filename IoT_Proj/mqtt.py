95a56f1b157551b14a0e5d3c4173496e990d0fd9
Mohit Sathya <40423054+Metabix@users.noreply.github.com>
2022-04-11 17:36:14 -0400

    Create mqtt.py

---
+++ IoT_Proj/mqtt.py
@@ -0,0 +1,73 @@
+import paho.mqtt.client as mqtt
+import codecs
+
+
+MQTT_ADDRESS = '172.16.105.2'
+
+MQTT_USER = 'mo'
+
+MQTT_PASSWORD = '1234567890'
+
+MQTT_TOPIC = 'home/+/+'
+
+
+
+
+
+def on_connect(client, userdata, flags, rc):
+
+    """ The callback for when the client receives a CONNACK response from the server."""
+
+    print('Connected with result code ' + str(rc))
+
+    client.subscribe(MQTT_TOPIC)
+
+
+
+
+
+def on_message(client, userdata, msg):
+
+    """The callback for when a PUBLISH message is received from the server."""
+    if msg.topic=="home/livingroom/temperature":
+        a=(msg.payload)
+        print(codecs.decode(a,'UTF-8'))
+        #print("temperature"+ ' ' + a)
+    
+    else:
+        b=(msg.payload)
+        print(codecs.decode(b,'UTF-8'))
+        #print("humidity"+ ' ' + b)
+
+
+
+
+
+
+def main():
+
+    mqtt_client = mqtt.Client()
+
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+
+    mqtt_client.on_connect = on_connect
+
+    mqtt_client.on_message = on_message
+
+
+
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+
+    mqtt_client.loop_forever()
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('MQTT to InfluxDB bridge')
+
+    main()
+
+
