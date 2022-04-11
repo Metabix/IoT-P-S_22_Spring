d31eba589079ac45b7b2d03db5d42d33832707d8
Mohit Sathya <40423054+Metabix@users.noreply.github.com>
2022-04-10 22:24:46 -0400

    Update ESP32.cpp

--- IoT_Proj/ESP32.cpp
+++ IoT_Proj/ESP32.cpp
@@ -1,30 +1,11 @@
 #include "mbedtls/aes.h"
 #include "PubSubClient.h" // Connect and publish to the MQTT broker
 
+
 // Code for the ESP32
 #include "WiFi.h" // Enables the ESP32 to connect to the local network (via WiFi)
 
 #include "SparkFun_Si7021_Breakout_Library.h" 
-
-void encrypt(char * plainText, char * key, unsigned char * outputBuffer){
- 
-  mbedtls_aes_context aes;
- 
-  mbedtls_aes_init( &aes );
-  mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
-  mbedtls_aes_crypt_ecb( &aes, MBEDTLS_AES_ENCRYPT, (const unsigned char*)plainText, outputBuffer);
-  mbedtls_aes_free( &aes );
-}
- 
-void decrypt(unsigned char * chipherText, char * key, unsigned char * outputBuffer){
- 
-  mbedtls_aes_context aes;
- 
-  mbedtls_aes_init( &aes );
-  mbedtls_aes_setkey_dec( &aes, (const unsigned char*) key, strlen(key) * 8 );
-  mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, (const unsigned char*)chipherText, outputBuffer);
-  mbedtls_aes_free( &aes );
-}
 
 Weather sensor;
 
@@ -34,7 +15,7 @@ const char* ssid = "Airwave-05-521B";                 // Your personal network S
 const char* wifi_password = ""; // Your personal network password
 
 // MQTT
-const char* mqtt_server = "172.16.105.1";  // IP of the MQTT broker
+const char* mqtt_server = "172.16.105.2";  // IP of the MQTT broker
 const char* humidity_topic = "home/livingroom/humidity";
 const char* temperature_topic = "home/livingroom/temperature";
 const char* mqtt_username = "mo"; // MQTT username
@@ -45,6 +26,26 @@ const char* clientID = "client_livingroom"; // MQTT client ID
 WiFiClient wifiClient;
 // 1883 is the listener port for the Broker
 PubSubClient client(mqtt_server, 1883, wifiClient); 
+
+void encrypt(char * plainText, char * key, unsigned char * outputBuffer){
+
+  mbedtls_aes_context aes;
+
+  mbedtls_aes_init( &aes );
+  mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
+  mbedtls_aes_crypt_ecb( &aes, MBEDTLS_AES_ENCRYPT, (const unsigned char*)plainText, outputBuffer);
+  mbedtls_aes_free( &aes );
+}
+
+void decrypt(unsigned char * chipherText, char * key, unsigned char * outputBuffer){
+
+  mbedtls_aes_context aes;
+
+  mbedtls_aes_init( &aes );
+  mbedtls_aes_setkey_dec( &aes, (const unsigned char*) key, strlen(key) * 8 );
+  mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, (const unsigned char*)chipherText, outputBuffer);
+  mbedtls_aes_free( &aes );
+}
 
 // Custom function to connet to the MQTT broker via WiFi
 void connect_MQTT(){
@@ -65,9 +66,6 @@ void connect_MQTT(){
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Connect to MQTT Broker
-  // client.connect returns a boolean value to let us know if the connection was successful.
-  // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
   if (client.connect(clientID, mqtt_username, mqtt_password)) {
     Serial.println("Connected to MQTT Broker!");
   }
@@ -80,56 +78,76 @@ void connect_MQTT(){
 void setup() {
   Serial.begin(115200);
   sensor.begin();
+
+  connect_MQTT();
 }
 
 void loop() {
-  connect_MQTT();
-  Serial.setTimeout(2000);
+Serial.setTimeout(2000);
 
-  float h = sensor.getRH();
-  float t = sensor.getTemp();
+float h = sensor.getRH();
+float t = sensor.getTemp();
 
-  Serial.print("Humidity:  ");
-  Serial.print(h);
-  Serial.println(" %");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C");
+char hc=char(h);
+char tc=char(t);
 
 
+Serial.print("Humidity:  ");
+Serial.print(h);
+Serial.println(" %");
+Serial.print("Temperature: ");
+Serial.print(t);
+Serial.println(" *C");
 
+mbedtls_aes_context aes;
+ 
+char * key = "abcdefghijklmnop";
+char *plainText = "24.43";
+unsigned char cipherTextOutput[16];
+unsigned char decipheredTextOutput[4];
+encrypt(plainText, key, cipherTextOutput);
+decrypt(cipherTextOutput, key, decipheredTextOutput);
+
+Serial.println("\nOriginal plain text:");
+Serial.println(plainText);
+
+Serial.println("\nCiphered text:");
+for (int i = 0; i < 16; i++) {
+
+  char str[3];
+
+  sprintf(str, "%02x", (int)cipherTextOutput[i]);
+  Serial.print(str);
+  }
+
+Serial.println("\n\nDeciphered text:");
+for (int i = 0; i < 5; i++) {
+    Serial.print((char)decipheredTextOutput[i]);
+  } 
+Serial.println();  
+
+
+// MQTT can only transmit strings
+String hs="Hum: "+String((float)h)+" % ";
+String ts="Temp: "+String((float)t)+" C ";
+
+// PUBLISH to the MQTT Broker (topic = Temperature, defined at the beginning)
+if (client.publish(temperature_topic, String(t).c_str())) 
+{
+  Serial.println("Temperature sent!");
+}
+else {Serial.println("Fail T");}
+
+
+// PUBLISH to the MQTT Broker (topic = Humidity, defined at the beginning)
+if (client.publish(humidity_topic, String(h).c_str())) 
+{
+  Serial.println("Humidity sent!");
+}
+else {Serial.println("Fail H");}
+
+//client.disconnect();  // disconnect from the MQTT broker
+client.connect(clientID, mqtt_username, mqtt_password);
+delay(1000); //delay(1000*60);       // print new values every 1 Minute  
   
-
-  // MQTT can only transmit strings
-  String hs="Hum: "+String((float)h)+" % ";
-  String ts="Temp: "+String((float)t)+" C ";
-
-  // PUBLISH to the MQTT Broker (topic = Temperature, defined at the beginning)
-  if (client.publish(temperature_topic, String(t).c_str())) {
-    Serial.println("Temperature sent!");
-  }
-  // Again, client.publish will return a boolean value depending on whether it succeded or not.
-  // If the message failed to send, we will try again, as the connection may have broken.
-  //else {
-  //  Serial.println("Temperature failed to send. Reconnecting to MQTT Broker and trying again");
-  //  client.connect(clientID, mqtt_username, mqtt_password);
-  //  delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
-  //  client.publish(temperature_topic, String(t).c_str());
-  // }
-
-  // PUBLISH to the MQTT Broker (topic = Humidity, defined at the beginning)
-  if (client.publish(humidity_topic, String(h).c_str())) {
-    Serial.println("Humidity sent!");
-  }
-  // Again, client.publish will return a boolean value depending on whether it succeded or not.
-  // If the message failed to send, we will try again, as the connection may have broken.
-  //else {
-  //  Serial.println("Humidity failed to send. Reconnecting to MQTT Broker and trying again");
-  //  client.connect(clientID, mqtt_username, mqtt_password);
-  //  delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
-  //  client.publish(humidity_topic, String(h).c_str());
-  //}
-  client.disconnect();  // disconnect from the MQTT broker
-  delay(500); //delay(1000*60);       // print new values every 1 Minute
-  connect_MQTT();
 }
