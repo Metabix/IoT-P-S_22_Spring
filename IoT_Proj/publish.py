4255d28096497dee7d1b2ad1b561a3eac895a951
Mohit Sathya <40423054+Metabix@users.noreply.github.com>
2022-04-12 23:53:05 +0100

    Update publish.py

--- IoT_Proj/publish.py
+++ IoT_Proj/publish.py
@@ -1,17 +1,8 @@
-#aws stuff
-# importing libraries
-import paho.mqtt.client as paho
-import os
-import socket
-import ssl
-import random
-import string
-import json
-from time import sleep
-from random import uniform
-
-#esp32-pi stuff
 import paho.mqtt.client as mqtt
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import codecs
+import time
+
 
 MQTT_ADDRESS = '172.16.105.2'
 
@@ -20,6 +11,9 @@ MQTT_USER = 'mo'
 MQTT_PASSWORD = '1234567890'
 
 MQTT_TOPIC = 'home/+/+'
+
+
+
 
 
 def on_connect(client, userdata, flags, rc):
@@ -32,101 +26,68 @@ def on_connect(client, userdata, flags, rc):
 
 
 
+
+
 def on_message(client, userdata, msg):
+    global a
+    global b
 
     """The callback for when a PUBLISH message is received from the server."""
     if msg.topic=="home/livingroom/temperature":
         a=(msg.payload)
-        print(codecs.decode(a,'UTF-8'))
+        print("temp",a.decode('cp437'))
         #print("temperature"+ ' ' + a)
     
     else:
         b=(msg.payload)
-        print(codecs.decode(b,'UTF-8'))
+        print("humid",b.decode('cp437'))
         #print("humidity"+ ' ' + b)
 
 
 
+def main():
+    while True:
+        mqtt_client = mqtt.Client()
 
+        mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
 
+        mqtt_client.on_connect = on_connect
 
-mqtt_client = mqtt.Client()
+        mqtt_client.on_message = on_message
 
-mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-
-mqtt_client.on_connect = on_connect
-
-mqtt_client.on_message = on_message
-
-
-
-mqtt_client.connect(MQTT_ADDRESS, 1883)
-
-
-
-
-
-
-\
-#----------------------------------------- ESP data collected --------------------------------------------------
-
- 
-connflag = False
- 
-def awson_connect(client, userdata, flags, rc):                # func for making connection
-    global connflag
-    print("Connected to AWS")
-    connflag = True
-    print("Connection returned result: " + str(rc) )
- 
-def awson_message(client, userdata, msg):                      # Func for Sending msg
-    print(msg.topic+" "+str(msg.payload))
     
-    
- 
-#def on_log(client, userdata, level, buf):
-#    print(msg.topic+" "+str(msg.payload))
- 
-mqttc = paho.Client()                                       # mqttc object
-mqttc.on_connect = awson_connect                               # assign on_connect func
-mqttc.on_message = awson_message                               # assign on_message func
-#mqttc.on_log = on_log
 
-#### Change following parameters #### 
-awshost = "aelti3spevku9-ats.iot.us-west-2.amazonaws.com"      # Endpoint
-awsport = 8883                                              # Port no.   
-clientId = "AgrIoT_client"                                     # Thing_Name
-thingName = "AgrIoT_client"                                    # Thing_Name
-caPath = "/home/pi/Downloads/AmazonRootCA1.pem"                                      # Root_CA_Certificate_Name
-certPath = "/home/pi/Downloads/AgrIoT-certificate.pem.crt"                            # <Thing_Name>.cert.pem
-keyPath = "/home/pi/Downloads/AgrIoT-private.pem.key"                         # <Thing_Name>.private.key
- 
-mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)  # pass parameters
- 
-mqttc.connect(awshost, awsport, keepalive=60)               # connect to aws server
- 
-mqttc.loop_start()                                          # Start the loop
- 
-while 1==1:
-    sleep(5)
-    if connflag == True:
-        randomNumber = uniform(20.0,25.0)
-        temperature_string= on_message
-        humidity_string= on_message
-        paylodmsg0="{"
-        paylodmsg1 = "\"temperature\": \""
-        paylodmsg2 = "\"humidity\": \""
-        paylodmsg3="\"}"
-        paylodmsg = "{} {} {} {} {} {}".format(paylodmsg0, paylodmsg1,temperature_string ,paylodmsg2, humidity_string, paylodmsg3)
-        paylodmsg = json.dumps(paylodmsg) 
-        paylodmsg_json = json.loads(paylodmsg)       
-        mqttc.publish("ElectronicsInnovation", paylodmsg_json , qos=1)        # topic: temperature # Publishing Temperature values
-        print("msg sent: ElectronicsInnovation" ) # Print sent temperature msg on console
-        print(paylodmsg_json)
+        mqtt_client.connect(MQTT_ADDRESS, 1883)
 
-    else:
-        print("waiting for connection...")
+        mqtt_client.loop_start()
+        time.sleep(1) 
+        mqtt_client.disconnect()
+        mqtt_client.loop_stop()
         
-mqtt_client.loop_forever()
+        myMQTTClient = AWSIoTMQTTClient("AgrIoTClientID") #random key, if another connection using the same key is opened the previous one is auto closed by AWS IOT
+        myMQTTClient.configureEndpoint("aelti3spevku9-ats.iot.us-west-2.amazonaws.com", 8883)
+        myMQTTClient.configureCredentials("/home/pi/Downloads/AmazonRootCA1.pem",
+                                          "/home/pi/Downloads/AgrIoT-private.pem.key",
+                                          "/home/pi/Downloads/AgrIoT-certificate.pem.crt")
+        myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
+        myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
+        myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
+        myMQTTClient.configureMQTTOperationTimeout(5) # 5 sec
+        print ('Initiating Realtime Data Transfer From Raspberry Pi...')
+        myMQTTClient.connect()
+        myMQTTClient.publish(
+            topic="sensor",
+            QoS=1,
+            payload='{"Temperature":"'+str(a)+'","humidity":"'+str(b)+'"}')
+        myMQTTClient.disconnect()
+
+
+
+if __name__ == '__main__':
+
+    print('MQTT to InfluxDB bridge')
+
+    main()
+
 
 
